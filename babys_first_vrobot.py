f14b3699e4f3f748c9cd473542d305838a1c292d
Tar6269 <114016175+Tar6269@users.noreply.github.com>
2024-04-16 14:05:43 -0600

    here vro
    take this vro

--- babys_first_vrobot.py
+++ babys_first_vrobot.py
@@ -2,7 +2,9 @@ import splinter
 
 first_vrobot = splinter.Browser('chrome')
 
-first_vrobot.visit('google.ca')
+first_vrobot.visit('https://google.ca')
 
 
-first_vrobot.screenshot()
+first_vrobot.screenshot('test', '.png', True, False)
+
+first_vrobot.quit()
